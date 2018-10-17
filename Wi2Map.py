c6b055a863b16ab57fe2523ea4ede63aa3359377
Tanisuke Goro <keisi1100525@outlook.jp>
2018-10-17 14:57:13 +0900

    ソースコードをpep8に準拠させた。 fix code conformed to pep-8.

--- Wi2Map.py
+++ Wi2Map.py
@@ -5,18 +5,23 @@ import re
 import sys
 import os
 import math
-
+# This program is getting "Wi2 300" wi-fi service areas.
+# the following describes the configuration of the program.
+# FUNCTION
+# read_table
 # constant parameter
 # END = 47 * 8
 MAX_LEN = 30
-URL_ID1 = '&search_type=xlos&pageID=0/ClassSkeletownController.php?action=area&emx=1&isp=&pageID='
+URL_ID1 = '&search_type=xlos&pageID=0/ClassSkeletownController.php?'\
+           'action=area&emx=1&isp=&pageID='
 # ------
+
 
 def ReadTable(url):
     # set the initial page No.
     last_page = 1
     url_ini = url + str(last_page)
-    # 各カテゴリにおけるページ数の取得
+    # get the number of pages for each category
     res = requests.get(url_ini)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "html.parser")
@@ -39,19 +44,20 @@ def ReadTable(url):
     df = pd.read_html(url_ini)
     df[0] = df[0].drop(1)
 
-    out_put_file = "/Users/abekeishi/Public/Programing/Python3/Wi2Map/out_put_data/" \
-                    + df[0].iat[0,1] + '_' + df[0].iat[1,1] +".csv"
+    out_put_file = "/Users/abekeishi/Public/Programing/Python3/Wi2Map" \
+                "/out_put_data/" + df[0].iat[0, 1] + '_' + df[0].iat[1, 1] \
+                + ".csv"
     # print('Out put > ' + df[0].iat[0,1] + '_' + df[0].iat[1,1] +".csv")
-    print("\033[36mPrefecture   : "+ df[0].iat[0,1]+"\033[0m")
-    print("\033[36mCategory     : "+ df[0].iat[1,1]+"\033[0m")
-    print("\033[36mPage         : "+ str(last_page) +"\033[0m")
+    print("\033[36mPrefecture   : " + df[0].iat[0, 1] + "\033[0m")
+    print("\033[36mCategory     : " + df[0].iat[1, 1] + "\033[0m")
+    print("\033[36mPage         : " + str(last_page) + "\033[0m")
     progress_bar(1, last_page)
     # get the 1st page data
     df = pd.read_html(url_ini)
     df[1] = df[1].drop(columns='地図')
     df[1].to_csv(out_put_file, index=False)
     # get the all page data, if exist some pages
-    if last_page > 1 :
+    if last_page > 1:
         # get data 2nd page to last page
         for i in range(2, last_page + 1):
             url_page = url + str(i)
@@ -59,8 +65,9 @@ def ReadTable(url):
             df[1] = df[1].drop(columns='地図')
             progress_bar(i, last_page)
             # print(i)
-            df[1].to_csv(out_put_file, mode = 'a', header =False, index=False)
+            df[1].to_csv(out_put_file, mode='a', header=False, index=False)
     fresh()
+
 
 # Progress Bar function
 # progress_bar --> get_progressbar_str --> fresh
@@ -70,9 +77,12 @@ def get_progressbar_str(progress):
             ('>' if BAR_LEN < MAX_LEN else '') +
             ' ' * (MAX_LEN - BAR_LEN) +
             '] %.1f%%' % (progress * 100.))
+
+
 def fresh():
     sys.stderr.write('\n')
     sys.stderr.flush()
+
 
 def progress_bar(curr_progress, end):
     progress = 1.0 * curr_progress / end
@@ -82,11 +92,11 @@ def progress_bar(curr_progress, end):
 
 
 # Main function
-if __name__ == "__main__" :
+if __name__ == "__main__":
     # Switch the standard output to file output
     # fo = open('Wi2log.txt', 'w')
     # sys.stdout = fo
-    progress_count= 0
+    progress_count = 0
     # define the url parts
     for prefecture_id in range(1, 48):
         url_head = 'http://300.wi2.co.jp/area/1/?prefecture=' + str(prefecture_id)
@@ -95,25 +105,25 @@ if __name__ == "__main__" :
 
             if category_id == 1:
                 str_small_category = ('str_small_category=a%253A8%253A%257Bi%253A0%253Bs%253A3%253A%25221%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25221%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25221%252C4%2522%253Bi%253A3%253Bs%253A3%253A%25221%252C6%2522%253Bi%253A4%253Bs%253A3%253A%25221%252C7%2522%253Bi%253A5%253Bs%253A3%253A%25221%252C8%2522%253Bi%253A6%253Bs%253A3%253A%25221%252C9%2522%253Bi%253A7%253Bs%253A4%253A%25221%252C10%2522%253B%257D')
-            elif category_id ==2:
+            elif category_id == 2:
                 str_small_category = ('str_small_category=a%253A5%253A%257Bi%253A0%253Bs%253A3%253A%25222%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25222%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25222%252C3%2522%253Bi%253A3%253Bs%253A3%253A%25222%252C4%2522%253Bi%253A4%253Bs%253A3%253A%25222%252C5%2522%253B%257D')
-            elif category_id ==3:
+            elif category_id == 3:
                 str_small_category = ('str_small_category=a%253A10%253A%257Bi%253A0%253Bs%253A3%253A%25223%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25223%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25223%252C3%2522%253Bi%253A3%253Bs%253A3%253A%25223%252C4%2522%253Bi%253A4%253Bs%253A3%253A%25223%252C5%2522%253Bi%253A5%253Bs%253A3%253A%25223%252C6%2522%253Bi%253A6%253Bs%253A3%253A%25223%252C7%2522%253Bi%253A7%253Bs%253A3%253A%25223%252C9%2522%253Bi%253A8%253Bs%253A4%253A%25223%252C10%2522%253Bi%253A9%253Bs%253A3%253A%25223%252C8%2522%253B%257D')
-            elif category_id ==4:
+            elif category_id == 4:
                 str_small_category = ('str_small_category=a%253A5%253A%257Bi%253A0%253Bs%253A3%253A%25224%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25224%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25224%252C3%2522%253Bi%253A3%253Bs%253A3%253A%25224%252C4%2522%253Bi%253A4%253Bs%253A3%253A%25224%252C5%2522%253B%257D')
-            elif category_id ==5:
+            elif category_id == 5:
                 str_small_category = ('str_small_category=a%253A10%253A%257Bi%253A0%253Bs%253A3%253A%25225%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25225%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25225%252C3%2522%253Bi%253A3%253Bs%253A3%253A%25225%252C4%2522%253Bi%253A4%253Bs%253A3%253A%25225%252C5%2522%253Bi%253A5%253Bs%253A3%253A%25225%252C6%2522%253Bi%253A6%253Bs%253A3%253A%25225%252C7%2522%253Bi%253A7%253Bs%253A3%253A%25225%252C9%2522%253Bi%253A8%253Bs%253A4%253A%25225%252C10%2522%253Bi%253A9%253Bs%253A3%253A%25225%252C8%2522%253B%257D')
-            elif category_id ==6:
+            elif category_id == 6:
                 str_small_category = ('str_small_category=a%253A5%253A%257Bi%253A0%253Bs%253A3%253A%25226%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25226%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25226%252C3%2522%253Bi%253A3%253Bs%253A3%253A%25226%252C5%2522%253Bi%253A4%253Bs%253A3%253A%25226%252C4%2522%253B%257D')
-            elif category_id ==7:
+            elif category_id == 7:
                 str_small_category = ('str_small_category=a%253A3%253A%257Bi%253A0%253Bs%253A3%253A%25227%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25227%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25227%252C3%2522%253B%257D')
-            elif category_id ==8:
+            elif category_id == 8:
                 str_small_category = ('str_small_category=a%253A6%253A%257Bi%253A0%253Bs%253A3%253A%25228%252C1%2522%253Bi%253A1%253Bs%253A3%253A%25228%252C2%2522%253Bi%253A2%253Bs%253A3%253A%25228%252C3%2522%253Bi%253A3%253Bs%253A3%253A%25228%252C4%2522%253Bi%253A4%253Bs%253A3%253A%25228%252C5%2522%253Bi%253A5%253Bs%253A3%253A%25228%252C6%2522%253B%257D')
             else:
                 print ('Error category_id')
             # same parameter
             progress_count += 1
-            URL_path = url_head + Category +str_small_category + URL_ID1
+            URL_path = url_head + Category + str_small_category + URL_ID1
             # print (str(prefecture_id) + " :" + str(category_id))
             ReadTable(URL_path)
             # progress_bar(progress_count)
@@ -121,8 +131,7 @@ if __name__ == "__main__" :
 fresh()
 
 
-        # URLパスを渡して読み込み関数の呼び出し。
-
+# URLパスを渡して読み込み関数の呼び出し。
 # ページの読み込みの数に関して
 # もし、next pageタグがあればlast pageタグを探索(単に"次へ"という文字列の有無で判断してもいいかもしれない)
 # あればlast pageの文字を取得。最大値とする。
